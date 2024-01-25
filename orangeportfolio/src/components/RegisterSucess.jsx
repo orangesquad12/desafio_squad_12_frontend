bed021480ff4716d90bb01548aea8edd01e95e39
Lucas Jair Bento <lucasjair4@hotmail.com>
2024-01-25 15:32:21 -0300

    feat: finished register sucess component

---
+++ orangeportfolio/src/components/RegisterSucess.jsx
@@ -0,0 +1,10 @@
+import Image from "../assets/img/CheckCircleOutlined.png"
+function RegisterSucess(){
+    return (
+        <div className="registersucess">
+            <img src={Image} alt="" />
+            <p>Cadastro feito com sucesso</p>
+        </div>
+    )
+}
+export default RegisterSucess
--- orangeportfolio/src/main.jsx
+++ orangeportfolio/src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Register from './Register.jsx'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Register />
+    <Register/>
   </React.StrictMode>,
 )

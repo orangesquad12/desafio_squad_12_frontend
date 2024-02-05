3774d24d2ef70dd2ca0ad362ee4dba2fe93cc131
Lucas Jair Bento <lucasjair4@hotmail.com>
2024-02-05 19:34:08 -0300

    reafactoring: error

--- orangeportfolio/src/routes/App.jsx
+++ orangeportfolio/src/routes/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes,Route } from "react-router-dom";
 import LoginM from "../components/LoginM";
 import RegisterM from "../components/RegisterM";
 import HomePageM from "../components/HomePageM";
-import Discovery from "../components/discovery";
 import { AuthProvider } from '../contexts/AuthContext';
 
 

0f1acabf3ed300ff176142c75f6ff2a6994c448c
Lucas Jair Bento <lucasjair4@hotmail.com>
2024-02-05 19:38:18 -0300

    refactor: import discovery

--- orangeportfolio/src/routes/App.jsx
+++ orangeportfolio/src/routes/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes,Route } from "react-router-dom";
 import LoginM from "../components/LoginM";
 import RegisterM from "../components/RegisterM";
 import HomePageM from "../components/HomePageM";
+import Discovery from "../components/Discovery";
 import { AuthProvider } from '../contexts/AuthContext';
 
 
